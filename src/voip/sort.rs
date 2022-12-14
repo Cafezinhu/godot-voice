c1c048de30698d86a7628709e90f87b79482194d
Aerton <aertoncastro@gmail.com>
2022-12-14 12:22:49 -0300

    sorting is back

--- src/voip/sort.rs
+++ src/voip/sort.rs
@@ -1,34 +1,37 @@
-pub fn quick_sort(mut pool: Vec<Vec<u8>>) -> Vec<Vec<u8>>{
-    if pool.len() == 0 {
-        return pool;
+use super::VoicePacket;
+
+pub fn quick_sort(mut voice_packets: Vec<VoicePacket>) -> Vec<VoicePacket>{
+    
+    if voice_packets.len() == 0 {
+        return voice_packets;
     }
 
-    let mut pivot = pool.len();
+    let mut pivot = voice_packets.len();
     let mut i = 0;
     let mut j = 1;
 
     while j != pivot {
-        if get_id(pool[j - 1].clone()) < get_id(pool[pivot - 1].clone()) {
+        if voice_packets[j - 1].id < voice_packets[pivot - 1].id {
             i += 1;
-            let swapping_buffer = pool[j - 1].clone();
-            pool[j - 1] = pool[i - 1].clone();
-                pool[i - 1] = swapping_buffer;
+            let swapping_buffer = voice_packets[j - 1].clone();
+            voice_packets[j - 1] = voice_packets[i - 1].clone();
+                voice_packets[i - 1] = swapping_buffer;
         }
         j += 1;
     }
-    let saved_pivot = pool[pivot - 1].clone();
+    let saved_pivot = voice_packets[pivot - 1].clone();
 
-    pool.remove(pivot - 1);
+    voice_packets.remove(pivot - 1);
 
     pivot = i;
 
-    pool.insert(pivot, saved_pivot);
+    voice_packets.insert(pivot, saved_pivot);
 
-    if pool.len() < 3 {
-        return pool;
+    if voice_packets.len() < 3 {
+        return voice_packets;
     }
 
-    let split = pool.split_at(pivot);
+    let split = voice_packets.split_at(pivot);
 
     let left = quick_sort(split.0.to_vec());
     let mut right = quick_sort(split.1.to_vec());
@@ -37,10 +40,4 @@ pub fn quick_sort(mut pool: Vec<Vec<u8>>) -> Vec<Vec<u8>>{
     new_pool.append(&mut right);
 
     new_pool
-}
-
-fn get_id(buffer: Vec<u8>) -> u32 {
-    u32::from_le_bytes(
-        buffer[..4].try_into().unwrap(),
-    )
 }
